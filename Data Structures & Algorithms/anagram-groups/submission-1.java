8aab2f22e40dbd72e333e0788e71b982e6917c22
LasimaP <38665842+LasimaP@users.noreply.github.com>
2026-06-03 12:31:54 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        List<List<String>> anagrams = new ArrayList<>();
+        HashMap<List<Integer>, List<String>> freqMap = new HashMap<>();
+
+        for (String s : strs) {
+            List<Integer> key = stringToFreqList(s);
+            if (!freqMap.containsKey(key)) {
+                freqMap.put(key, new ArrayList<>());
+            }
+
+            freqMap.get(key).add(s);
+        }
+
+        for (List<Integer> key : freqMap.keySet()) {
+            anagrams.add(freqMap.get(key));
+        }
+        return anagrams;
+    }
+
+    public static List<Integer> stringToFreqList(String s) {
+        int[] freq = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        return Arrays.stream(freq)
+                 .boxed()
+                 .collect(Collectors.toList());
+    }
+}
